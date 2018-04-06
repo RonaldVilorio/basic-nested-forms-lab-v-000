6fd761819fa8bcde2d600758238b46900ac31ba9
ronald vilorio <soccer23415@gmail.com>
2018-04-06 18:19:34 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -23,6 +23,6 @@ class RecipesController < ApplicationController
   end
   private
   def recipe_params
-    params.require(:recipe).permit(:title,ingredients: [])
+    params.require(:recipe).permit(:title,:ingredients_attributes)
   end
 end
